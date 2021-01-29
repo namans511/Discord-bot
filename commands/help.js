07f3711c4d1b07f57886edfa6eae0afec8bd7df3
Naman Srivastava <srivastava.naman511@gmail.com>
2021-01-29 11:46:33 +0530

    saving current state

--- commands/help.js
+++ commands/help.js
@@ -51,7 +51,8 @@ module.exports = {
     if (command.usage)
       data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
 
-    data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
+    if (command.cooldown)
+      data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
 
     message.channel.send(data, { split: true });
   },
--- commands/join.js
+++ commands/join.js
@@ -1,12 +1,27 @@
+const { Collection } = require("discord.js");
+const ytdl = require('ytdl-core');
+
 module.exports = {
   name: "join",
   description: "join the vc you're in",
   async execute(message, args) {
-    if (message.member.voice.channel) {
-      const connection = await message.member.voice.channel.join();
-      return message.reply("kar liya join ✅");
-    } else {
-      return message.reply("saale vc to join karlo pehele ❌");
+    try {
+      if (message.guild && message.member.voice.channel) {
+        const connection = await message.member.voice.channel.join();
+        message.reply("kar liya join ✅");
+        const stream = ytdl("https://www.youtube.com/watch?v=_t46p_nFruU", {
+          filter: "audioonly",
+        });
+        const dispatcher = connection.play(stream);
+
+        dispatcher.on("finish", () => voiceChannel.leave());
+      } else {
+        return message.reply("saale vc to join karlo pehele ❌");
+      }
+    }
+    catch(error) {
+      console.error(error);
+      return message.reply("sorry bhaiya nahi ho paaya");
     }
   },
 };
